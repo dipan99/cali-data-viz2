d65c534aa2fda4f20c1249f75e775b7d507c0578
Dipan Bag <dipanbag99@gmail.com>
2025-04-07 23:04:55 -0500

    more charts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
     import { base } from "$app/paths";
 </script>
 
-<div class="container mt-5">
-    <p>Visualizations</p>
+<div class="container mt-5 border rounded p-3">
+    <p><strong>Visualizations</strong> </p>
     <div class="d-flex flex-column gap-2">
         <a href="{base}/linechart">California Wildfires Line Chart</a>
         <a href="{base}/scatterchart">Scatter Chart</a>
@@ -13,6 +13,14 @@
         <a href="{base}/parallelcoordinatesplot">Parallel Coordinates</a>
         <a href="{base}/multilayermap">Multilayer Map</a>
         <a href="{base}/brushedmultilayermap">Brushed Multilayer Map</a>
+        <a href="{base}/boxplot">Box Plot</a>
+        <a href="{base}/dualaxislinechart">Dual Axis Line Chart</a>
+        <a href="{base}/spider">Spider Chart</a>
     </div>
+    
+</div>
+
+<div class="container mt-3 border rounded p-3 pb-0">
+    <p><strong>The California Story</strong> &#40;<i>coming soon!</i>&#41;</p>
     
 </div>
---
+++ src/routes/boxplot/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Back from "$lib/Back.svelte";
+    import BoxPlot from "$lib/BoxPlot.svelte";
+</script>
+<div class="container">
+    <Back />
+    <h1>California Wildfires - BoxPlot</h1>
+    <BoxPlot />
+</div>
